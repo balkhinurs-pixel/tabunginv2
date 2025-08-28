e99f82c164f3fd6f31e2f7288164279b57fd7fd3
Balkhi Nurs <Balkhinurs@gmail.com>
2025-08-28 17:14:48 +0000

    solusi error seperti ini saat deploy ke vercel
    [00:12:33.694] Running b

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,15 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl) {
+  throw new Error("Supabase URL not found. Please add NEXT_PUBLIC_SUPABASE_URL to your environment variables.");
+}
+
+if (!supabaseAnonKey) {
+  throw new Error("Supabase anon key not found. Please add NEXT_PUBLIC_SUPABASE_ANON_KEY to your environment variables.");
+}
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
