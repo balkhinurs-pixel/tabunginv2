a5db77f0dbeb62e103aa1e64e3b2e76c0b2fc7e9
Balkhi Nurs <Balkhinurs@gmail.com>
2025-12-20 07:20:47 +0000

    pas saya coba buka tidak muncul popup install pwa nya?

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,29 @@
+
 /** @type {import('next').NextConfig} */
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'api.qrserver.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-            }
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.qrserver.com',
+        port: '',
+        pathname: '/v1/create-qr-code/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
