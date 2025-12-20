b32e5c9e7eff1405bac55cf731dd35e59ba6962b
Balkhi Nurs <Balkhinurs@gmail.com>
2025-12-20 07:25:29 +0000

    error seperti ini Error: file:///home/user/studio/next.config.mjs:3 cons

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,28 @@
 
-/** @type {import('next').NextConfig} */
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
 });
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'api.qrserver.com',
-        port: '',
-        pathname: '/v1/create-qr-code/**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
       },
     ],
   },
 };
 
-module.exports = withPWA(nextConfig);
+export default withPWA(nextConfig);
